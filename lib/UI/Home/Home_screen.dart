a75bd441cd130b61b764517691d080db22cdbdbf
srbshameer098 <srbshameer098@gmail.com>
2024-01-12 18:03:52 +0530

    lotte_animation_completed

--- lib/UI/Home/Home_screen.dart
+++ lib/UI/Home/Home_screen.dart
@@ -1,5 +1,10 @@
+import 'package:firebase/UI/Home/page2.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../RoundButton.dart';
+bool loading=false;
 class Home_screen extends StatefulWidget {
   const Home_screen({Key? key}) : super(key: key);
 
@@ -14,6 +19,37 @@ class _Home_screenState extends State<Home_screen> {
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
         title: Center(child: Text('Home')),
+      ),
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: 40),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to flutter World',style: TextStyle(fontSize: 24),),
+            // Container(width: 200,
+            //     height: 200,
+            //     child: Lottie.asset(
+            //       'assets/animi.json',
+            //       width: 200,
+            //       height: 200,
+            //       fit: BoxFit.fill,)
+            //     ),
+
+            LoadingButton(title: 'play', onTap: () {
+              setState(() {
+                loading = true;
+              });
+              Future.delayed(Duration(seconds: 5),(){
+                setState(() {
+                  loading=false;
+                });
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>page2()));
+              });
+
+            },)
+
+          ],
+        ),
       ),
     );
   }
---
+++ lib/UI/Home/page2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+class page2 extends StatefulWidget {
+  const page2({Key? key}) : super(key: key);
+
+  @override
+  State<page2> createState() => _page2State();
+}
+
+class _page2State extends State<page2> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('page2',)
+      ),
+    );
+  }
+}
