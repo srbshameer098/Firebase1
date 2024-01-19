eccbcc213f74b56f03ed0580dbeadef3890f0316
srbshameer098 <srbshameer098@gmail.com>
2024-01-19 17:30:19 +0530

    fetchData

--- lib/UI/Home/Home_screen.dart
+++ lib/UI/Home/Home_screen.dart
@@ -10,7 +10,9 @@ import 'package:lottie/lottie.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../Widgets/RoundButton.dart';
-bool loading=false;
+
+bool loading = false;
+
 class Home_screen extends StatefulWidget {
   const Home_screen({Key? key}) : super(key: key);
 
@@ -19,90 +21,85 @@ class Home_screen extends StatefulWidget {
 }
 
 class _Home_screenState extends State<Home_screen> {
-
   final auth = FirebaseAuth.instance;
   final ref = FirebaseDatabase.instance.ref('Post');
 
-  
-  
-  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.blueAccent,
         title: const Center(child: Text('Home')),
-     actions:  [
-       IconButton( onPressed: () {
-         auth.signOut().then((value){
-           Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage() ));
-         }).onError((error, stackTrace) {
-           Utils().toastMessage(error.toString());
-         });
-       }, icon: const Icon(Icons.logout_outlined)),
-       const SizedBox(width: 10,)
-     ],
+        actions: [
+          IconButton(
+              onPressed: () {
+                auth.signOut().then((value) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                }).onError((error, stackTrace) {
+                  Utils().toastMessage(error.toString());
+                });
+              },
+              icon: const Icon(Icons.logout_outlined)),
+          const SizedBox(
+            width: 10,
+          )
+        ],
       ),
-
       body: Column(
         children: [
-
-
-
-          Expanded(child: StreamBuilder(
+          Expanded(
+              child: StreamBuilder(
             stream: ref.onValue,
-            builder: (context,AsyncSnapshot<DatabaseEvent> sanpshot){
+            builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+              if (snapshot.connectionState==ConnectionState.waiting) {
+                return Center(
+                    child: SizedBox(
+                        width: 30,
+                        height: 30,
+                        child: CircularProgressIndicator()));
+              }
 
-              if(snapshot.hasData){
-
-              }else {
+                if (snapshot.hasData) {
+                  Map<dynamic, dynamic> map =
+                  snapshot.data!.snapshot.value as dynamic;
+                  List<dynamic> list = [];
+                  list.clear();
+                  list = map.values.toList();
+                  return ListView.builder(
+                      itemCount: snapshot.data!.snapshot.children.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(list[index]['title']),
+                        );
+                      });
 
               }
-              return ListView.builder(
-                itemCount: snapshot.data.snapshot.children.lenth,
-                  itemBuilder: (context,index){
-
-
-                return ListTile(
-                  title: Text('asdfr'),
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error"),
                 );
-              }
-              );
+              }else{return SizedBox();}
             },
           )),
           Expanded(
             child: FirebaseAnimatedList(
                 query: ref,
                 defaultChild: Text('Loading'),
-                itemBuilder: (context, snapshot, animation, index){
-
+                itemBuilder: (context, snapshot, animation, index) {
                   return ListTile(
-                      title:Text (snapshot.child('title').value.toString()),
+                    title: Text(snapshot.child('title').value.toString()),
                     subtitle: Text(snapshot.child('id').value.toString()),
-                    );
-            }
-            ),
+                  );
+                }),
           ),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
           // const Text('Welcome to flutter World',style: TextStyle(fontSize: 24),),
           // Container(width: 200,
@@ -127,33 +124,15 @@ class _Home_screenState extends State<Home_screen> {
           //   });
           //
           // },)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         ],
       ),
-
-
-      floatingActionButton: FloatingActionButton(onPressed: (){
-
-        Navigator.push(context, MaterialPageRoute(builder: (context) => AddPostScreen()));
-
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
         child: Icon(Icons.add),
       ),
-
     );
   }
 }
